d4bc6477e6d58313252fe1f73e6fea6fdb0f2948
Hani al Sabti <h.sabti87@gmail.com>
2021-11-15 21:33:23 +0100

    added functionality to the slides

--- app/src/components/Carousel.vue
+++ app/src/components/Carousel.vue
@@ -1,11 +1,113 @@
 <template>
   <div class="carousel">
-    <slot />
+    <slot :currentSlide="currentSlide" />
+    <div class="navigate">
+      <div class="toggle-page left">
+        <i @click="prevSlide()" class="fas fa-chevron-left"></i>
+      </div>
+      <div class="toggle-page right">
+        <i @click="nextSlide()" class="fas fa-chevron-right"></i>
+      </div>
+    </div>
+    <div class="pagination">
+      <span
+        v-for="(slide, index) in getSlideCount"
+        :key="index"
+        :class="{ active: index + 1 === currentSlide }"
+        @click="goToSlide(index)"
+      >
+      </span>
+    </div>
   </div>
 </template>
 
 <script>
-  export default {};
+  import { ref, onMounted } from 'vue';
+  export default {
+    setup() {
+      const currentSlide = ref(1);
+      const getSlideCount = ref(null);
+
+      const nextSlide = () => {
+        if (currentSlide.value === getSlideCount.value) {
+          currentSlide.value = 1;
+          return;
+        }
+        currentSlide.value += 1;
+      };
+      const prevSlide = () => {
+        if (currentSlide.value === 1) {
+          return;
+        }
+        currentSlide.value -= 1;
+      };
+
+      const goToSlide = (index) => {
+        currentSlide.value = index + 1;
+      };
+
+      setInterval(() => {
+        nextSlide();
+      }, 5000);
+
+      onMounted(() => {
+        getSlideCount.value = document.querySelectorAll('.slide').length;
+      });
+
+      return { currentSlide, nextSlide, prevSlide, getSlideCount, goToSlide };
+    },
+  };
 </script>
 
-<style></style>
+<style lang="scss">
+  .navigate {
+    padding: 0 16px;
+    height: 100%;
+    width: 100%;
+    position: absolute;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    .toggle-page {
+      display: flex;
+      flex: 1;
+    }
+    .right {
+      justify-content: flex-end;
+    }
+    i {
+      cursor: pointer;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      border-radius: 50%;
+      width: 40px;
+      height: 40px;
+      background-color: #6347c7;
+      color: #fff;
+    }
+  }
+  .pagination {
+    position: absolute;
+    bottom: 24px;
+    width: 100%;
+    display: flex;
+    gap: 16px;
+    justify-content: center;
+    align-items: center;
+
+    span {
+      cursor: pointer;
+      width: 20px;
+      height: 20px;
+      border-radius: 50%;
+      background-color: #fff;
+      box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1),
+        0 1px 2px 0 rgba(0, 0, 0, 0.06);
+    }
+    .active {
+      background-color: #6347c7;
+    }
+  }
+</style>
